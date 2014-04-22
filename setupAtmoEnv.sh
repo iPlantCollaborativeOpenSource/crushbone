8e895ece0953d41ceb95ca486ceefe7aacb2a8e8
Andre Mercer <amercer1@email.arizona.edu>
2014-04-22 14:30:20 -0700

    Add python cryptography dependency

--- setupAtmoEnv.sh
+++ setupAtmoEnv.sh
@@ -8,9 +8,12 @@ source ./configurationVariables.sh
 
 add-apt-repository -y ppa:chris-lea/node.js
 apt-get update
-apt-get install -y python-pip python-software-properties python g++ make postgresql-9.1 postgresql-server-dev-9.1 libpq-dev python-dev libldap2-dev libsasl2-dev python-m2crypto swig redis-server libssl-dev git nodejs > installLogs
+apt-get install -y python-pip libffi-dev python-software-properties python g++ make postgresql-9.1 postgresql-server-dev-9.1 libpq-dev python-dev libldap2-dev libsasl2-dev python-m2crypto swig redis-server libssl-dev git nodejs > installLogs
+
+#Don't remove sleep or hash -r.
 sleep 3
-pip install --upgrade pip 
+pip install --upgrade pip
+hash -r
 
 echo -e "\n" >> installLogs
 
