7b61602d2b4b7625e059f5c79e9b0d3c12b0759a
Steve Gregory <contact@steve-gregory.com>
2014-09-03 14:06:14 -0700

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-Options=$@
-OptNum=$#
-
 _usage() {
 echo "##### U S A G E : Help and ERROR ######"
 echo "$0 $Options"
@@ -23,78 +19,112 @@ if [ "$1" != "" ]; then
 echo "USAGE ERROR: $1"
 fi
 }
-## Initialize command line vars
-atmo_only=false
-tropo_only=false
-test_only=false
 
-branch_name=""
-working_dir=""
-virtualenv_dir=""
-server_name=""
+_swap_variables() {
+    atmo_only=${atmo_only:-false}
+    tropo_only=${tropo_only:-false}
+    test_only=${test_only:-false}
 
-## Collect command line vars
-while getopts ':atT-b:D:E:s:' OPTION ; do
-  case "$OPTION" in
-    a  ) atmo_only=true           ;;
-    b  ) branch_name="$OPTARG"    ;;
-    D  ) working_dir="$OPTARG"    ;;
-    E  ) virtualenv_dir="$OPTARG" ;;
-    h  ) _usage                   ;;   
-    s  ) server_name="$OPTARG"    ;;
-    t  ) tropo_only=true          ;;
-    T  ) test_only=true           ;;
-    -  ) [ $OPTIND -ge 1 ] && optind=$(expr $OPTIND - 1 ) || optind=$OPTIND
-         eval OPTION="\$$optind"
-         OPTARG=$(echo $OPTION | cut -d'=' -f2)
-         OPTION=$(echo $OPTION | cut -d'=' -f1)
-         case $OPTION in
-             --atmosphere_only ) atmosphere_only=true    ;;
-             --branch ) branch_name="$OPTARG"            ;; 
-             --working_dir ) working_dir="$OPTARG"       ;; 
-             --virtualenv ) virtualenv_dir="$OPTARG"     ;; 
-             --help ) _usage                             ;;
-             --server_name ) server_name="$OPTARG"       ;; 
-             --troposphere_only ) troposphere_only=true  ;;
-             --test ) test_only=true                     ;;
-             * )  _usage " Unknown long option provided: $OPTION:$OPTARG" ;;
-         esac
-       OPTIND=1
-       shift
-      ;;
-    ? )  _usage " Unknown short option provided: $OPTION:$OPTARG" ;;
-  esac
-done
-echo "---------------------------------------------------"
-echo "Your Configuration:"
-echo "Atmosphere Only: $atmo_only"
-echo "Troposphere Only: $tropo_only"
-echo "Test Mode: $test_only"
-echo "Branch Name: $branch_name"
-echo "Server Name: $server_name"
-echo "Source Code Dir: $working_dir"
-echo "Environment Dir: $virtualenv_dir"
-echo "---------------------------------------------------"
-exit 0
-## Source the configuration variables
+    branch_name=${branch_name:-${BRANCH_NAME:-"master"}}
+    server_name=${server_name:-${SERVERNAME:-"localhost"}}
+    working_dir=${working_dir:-${WORKSPACE:-"/opt/dev"}}
+    virtualenv_dir=${virtualenv_dir:-${VIRTUALENV:-"/opt/env"}}
+}
 
-## Override them with arguments
+main() {
 
-mkdir logs
-. src/01_configurationVariables.sh
-. src/02_dependencies.sh
-. src/03_pip_install.sh
-. src/04_postgres.sh
-. src/05_setuptools.sh
-. src/06_atmo_virtual_env.sh
-. src/07_m2cryptoconfiguration.sh
-. src/08_atmo_git_clone.sh
-#. src/pip_install_atmo_requirements.sh
-#. src/atmo_python_db_migrations.sh
-#. src/atmo_virtual_env_deactivate.sh
-#. src/apache_configuration.sh
-#. src/ssl_configuration.sh
-#. src/ssh_keys.sh
-#. src/start_atmosphere.sh
-#. src/troposphere_virtual_env.sh
-#. src/troposphere_setup.sh
+    Options=$@
+    OptNum=$#
+
+
+    ## Source the configuration variables
+    mkdir -p logs
+    . src/01_configurationVariables.sh
+
+
+    ## Initialize command line vars
+    atmo_only=false
+    tropo_only=false
+    test_only=false
+    
+    branch_name=""
+    working_dir=""
+    virtualenv_dir=""
+    server_name=""
+    
+    ## Collect command line vars
+    while getopts ':atT-b:D:E:s:' OPTION ; do
+      case "$OPTION" in
+        a  ) atmo_only=true           ;;
+        b  ) branch_name="$OPTARG"    ;;
+        D  ) working_dir="$OPTARG"    ;;
+        E  ) virtualenv_dir="$OPTARG" ;;
+        h  ) _usage                   ;;   
+        s  ) server_name="$OPTARG"    ;;
+        t  ) tropo_only=true          ;;
+        T  ) test_only=true           ;;
+        -  ) [ $OPTIND -ge 1 ] && optind=$(expr $OPTIND - 1 ) || optind=$OPTIND
+             eval OPTION="\$$optind"
+             OPTARG=$(echo $OPTION | cut -d'=' -f2)
+             OPTION=$(echo $OPTION | cut -d'=' -f1)
+             case $OPTION in
+                 --atmosphere_only ) atmosphere_only=true    ;;
+                 --branch ) branch_name="$OPTARG"            ;; 
+                 --working_dir ) working_dir="$OPTARG"       ;; 
+                 --virtualenv ) virtualenv_dir="$OPTARG"     ;; 
+                 --help ) _usage                             ;;
+                 --server_name ) server_name="$OPTARG"       ;; 
+                 --troposphere_only ) troposphere_only=true  ;;
+                 --test ) test_only=true                     ;;
+                 * )  _usage " Unknown long option provided: $OPTION:$OPTARG" ;;
+             esac
+           OPTIND=1
+           shift
+          ;;
+        ? )  _usage " Unknown short option provided: $OPTION:$OPTARG" ;;
+      esac
+    done
+    echo $branch_name
+    _swap_variables
+    echo $branch_name
+    
+    echo "---------------------------------------------------"
+    echo "Your Configuration:"
+    echo "Atmosphere Only: $atmo_only"
+    echo "Troposphere Only: $tropo_only"
+    echo "Test Mode: $test_only"
+    echo "Branch Name: $branch_name"
+    echo "Server Name: $server_name"
+    echo "Source Code Dir: $working_dir"
+    echo "Environment Dir: $virtualenv_dir"
+    echo "---------------------------------------------------"
+    exit 0
+}
+
+run_steps() {
+    
+    ## Override them with arguments
+    
+    
+    . src/02_dependencies.sh
+    . src/03_pip_install.sh
+    . src/04_postgres.sh
+    . src/05_setuptools.sh
+    . src/06_atmo_virtual_env.sh
+    . src/07_m2cryptoconfiguration.sh
+    . src/08_atmo_git_clone.sh
+    #. src/pip_install_atmo_requirements.sh
+    #. src/atmo_python_db_migrations.sh
+    #. src/atmo_virtual_env_deactivate.sh
+    #. src/apache_configuration.sh
+    #. src/ssl_configuration.sh
+    #. src/ssh_keys.sh
+    #. src/start_atmosphere.sh
+    #. src/troposphere_virtual_env.sh
+    #. src/troposphere_setup.sh
+}
+
+
+#EXECUTION PATH:
+main $@
+run_steps
--- src/01_configurationVariables.sh
+++ src/01_configurationVariables.sh
@@ -1,27 +1,34 @@
 #!/bin/bash -x
 
-# TODO Be sure to change this!
-SERVERNAME="SERVERNAMEHERE"
+###
+#
+# Configuration Variables: Set these BEFORE executing install.sh!
+#
+###
+
+SERVERNAME="myserver"
 
 #BRANCH NAME
 # This is allowed to be blank, or you can specify a branch and crushbone will pull it down
-BRANCH_NAME=""
+BRANCH_NAME="mybranch"
 
 #Variables for Postgres. Recommended names
-DBNAME=atmo_prod
-DBUSER=atmo_app
-DBPASSWORD=atmosphere
+DBNAME="atmo_prod"
+DBUSER="atmo_app"
+DBPASSWORD="atmosphere"
 
 #Variables for Virtualenv
-VIRTUAL_ENV_ATMOSPHERE="/opt/env/atmo"
+WORKSPACE="myspace"
+VIRTUALENV="myenv"
 
 #Variables for Atmosphere
-LOCATIONOFATMOSPHERE="/opt/dev/atmosphere"
+LOCATIONOFATMOSPHERE="$WORKSPACE/atmosphere"
+VIRTUAL_ENV_ATMOSPHERE="$VIRTUALENV/atmo"
 LOCATIONOFLOGS="$LOCATIONOFATMOSPHERE/logs"
 
 #Variables for Trophosphere
-LOCATIONOFTROPOSPHERE="/opt/dev/troposphere"
-VIRTUAL_ENV_TROPOSHERE="/opt/env/troposphere" 
+LOCATIONOFTROPOSPHERE="$WORKSPACE/troposphere"
+VIRTUAL_ENV_ATMOSPHERE="$VIRTUALENV/troposphere"
 
 #Variables for configuration
 #This specifies the location of configuration files(e.g. local.py, secrets.py, testing.py)
@@ -36,7 +43,7 @@ NAMEOFYOURSSLBUNDLECERTIFICATE="gd_bundle.crt"
 
 #Variables for SSH Key Configuration
 
-LOCATIONOFPERSONALKEYS="/root/.ssh"
+LOCATIONOFPERSONALKEYS="$INITIALINSTALLDIRECTORY/.ssh"
 
-LOCATIONOFTROPOSPHERELOCALFILE="/root"
-LOCATIONOFTROPOSPHEREKEY="/root"
+LOCATIONOFTROPOSPHERELOCALFILE="$INITIALINSTALLDIRECTORY"
+LOCATIONOFTROPOSPHEREKEY="$INITIALINSTALLDIRECTORY"
