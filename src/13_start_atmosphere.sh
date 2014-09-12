7c569f4290956c16b3565db3f37ead5d03a2ff81
Andre Mercer <amercer1@email.arizona.edu>
2014-09-12 15:05:56 -0700

    Remove typo 	modified:   src/13_start_atmosphere.sh

--- src/13_start_atmosphere.sh
+++ src/13_start_atmosphere.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -x
 
+this_filename=$(basename $BASH_SOURCE)
+output_for_logs="logs/$this_filename.log" 
+touch $output_for_logs
+
 ################################
 # Run Atmosphere!
 ################################
 
-service apache2 restart 2>> install.log
+
+service apache2 restart 2>> $output_for_logs
