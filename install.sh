ea9514dd48d2e855e90b762145654d61ee1b804d
Steve Gregory <contact@steve-gregory.com>
2015-09-01 11:40:16 -0700

    Install dev requirements in test or jenkins mode

--- install.sh
+++ install.sh
@@ -193,7 +193,7 @@ build_atmosphere() {
     . ./src/06_atmo_virtual_env.sh $atmo_virtualenv
     ./src/07_atmo_git_clone.sh $atmo_working_dir $branch_name
     ./src/08_atmo_setup.sh $setup_files_dir $atmo_working_dir $atmo_logs_dir $atmo_virtualenv $server_name $db_name $db_user $db_pass
-    ./src/09_pip_install_atmo_requirements.sh $atmo_working_dir $atmo_virtualenv
+    ./src/09_pip_install_atmo_requirements.sh $atmo_working_dir $atmo_virtualenv $jenkins $test_only
     ./src/10_atmo_python_db_migrations.sh $atmo_working_dir $atmo_virtualenv
     . src/14_virtual_env_deactivate.sh
     ./src/18_atmo_setup_new_relic.sh $setup_files_dir $atmo_working_dir
@@ -211,7 +211,7 @@ atmo_rebuild_jenkins() {
     . ./src/06_atmo_virtual_env.sh $atmo_virtualenv
     #Jenkins already has the correct atmosphere settings, no overwrites
     #required
-    ./src/09_pip_install_atmo_requirements.sh $atmo_working_dir $atmo_virtualenv
+    ./src/09_pip_install_atmo_requirements.sh $atmo_working_dir $atmo_virtualenv $jenkins $test_only
     ./src/10_atmo_python_db_migrations.sh $atmo_working_dir $atmo_virtualenv
     #Jenkins already has the correct apache settings, no overwrites
     #required
--- src/09_pip_install_atmo_requirements.sh
+++ src/09_pip_install_atmo_requirements.sh
@@ -17,11 +17,17 @@ main(){
   
   LOCATIONOFATMOSPHERE=$1
   VIRTUAL_ENV_ATMOSPHERE=$2
+  JENKINS=$3
+  TEST=$4
   "$VIRTUAL_ENV_ATMOSPHERE/bin/pip" install -U -r "$LOCATIONOFATMOSPHERE/requirements.txt" >> $output_for_logs
+  if [[ "$JENKINS" = "true" || "$TEST" = "true" ]]; then
+      # Required to run jenkins OR a test
+      "$VIRTUAL_ENV_ATMOSPHERE/bin/pip" install -U -r "$LOCATIONOFATMOSPHERE/dev_requirements.txt" >> $output_for_logs
+  fi
   "$VIRTUAL_ENV_ATMOSPHERE/bin/wheel" install-scripts celery
 
 }
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 4 ]; then
   echo "Illegal number of parameters" 2>> $output_for_logs
   echo $@ 2> $output_for_logs
   exit 01
